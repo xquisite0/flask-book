6453154c544bd093ed8b67e7bcbc1194b82e8957
Zhou Yuhang <zyuhang255@gmail.com>
2023-03-25 15:11:56 +0800

    create basic functionality of adding and retrieving

--- api/index.py
+++ api/index.py
@@ -1,15 +1,54 @@
-from flask import Flask, request, render_template
-import pymongo, os
+import flask, pymongo
+from flask import render_template, request
 
-app = Flask(__name__)
-client = pymongo.MongoClient(os.environ['URI'])
+app = flask.Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    #if request.method == 'GET':
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template("index.html")
+
+    client = pymongo.MongoClient("mongodb+srv://zyuhang255:zXTdx5KsVFcgWZ5t@flask-book.pfkfynq.mongodb.net/?retryWrites=true&w=majority")
+    db = client.get_database("books")
+    coll = db.get_collection("book")
+    Title = request.form['Title']
+    Status = request.form['Status']
+    Genre = request.form['Genre']
+    Rating = request.form['Rating']
+    Review = request.form['Review']
+    if Status == '0':
+        Rating = "-"
+        Review = "-"
+
+    coll.insert_one({"Title": Title, "Status": Status, "Genre": Genre, "Rating": Rating, "Review": Review})
+
+    client.close()
+
     return render_template("index.html")
-    #return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/view")
+def view(): 
+
+    client = pymongo.MongoClient("mongodb+srv://zyuhang255:zXTdx5KsVFcgWZ5t@flask-book.pfkfynq.mongodb.net/?retryWrites=true&w=majority")
+    db = client.get_database("books")
+    coll = db.get_collection("book")
+    cur = coll.find()
+
+    books = []
+    for row in cur:
+        book = []
+        book.append(row['Title'])
+        if row['Status'] == 0:
+            book.append("Reading")
+        else:
+            book.append("Read")
+        book.append(row['Genre'])
+        book.append(row['Rating'])
+        book.append(row['Review'])
+        books.append(book)
+    client.close()
+    return render_template("view.html", books=books)
+
+if __name__ == '__main__':
+    app.run()
